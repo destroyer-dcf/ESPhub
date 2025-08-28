d2ded84632692cd63139294b3bb6d2653fbc623d
destroyer <destroyer-dcf@gmail.com>
2025-08-28 11:42:47 +0200

    add compilation files

--- compile.sh
+++ compile.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-export PICO_SDK_PATH=/home/destroyer/Projects/ESPhub/pico-sdk
+if [ ! -f "$PWD/CMakeLists.txt" ]; then
+    echo -e "\033[0;31mError: No se encontró CMakeLists.txt en $PWD\033[0m"
+    exit 1
+fi
 
+export PICO_SDK_PATH=/home/destroyer/Projects/ESPhub/pico-sdk
+echo "Me han ejecutado desde: $PWD"
 echo "PICO_SDK_PATH ha sido configurado a $PICO_SDK_PATH"
 PROJECT_DIR="."
 BUILD_DIR="$PROJECT_DIR/build"
@@ -16,9 +21,9 @@ fi
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 
-cmake -Wno-dev -DCMAKE_BUILD_TYPE=Debug .. 
+cmake -Wno-dev -DCMAKE_BUILD_TYPE=Debug ..
 make
 cd ..
 
 
-$PICO_SDK_PATH/openocd/0.12.0+dev/openocd.exe -s /Users/destroyer/.pico-sdk/openocd/0.12.0+dev/scripts -f /home/destroyer/Projects/ESPhub/pico-sdk/openocd/0.12.0+dev/scripts/interface/cmsis-dap.cfg -f /home/destroyer/Projects/ESPhub/pico-sdk/openocd/0.12.0+dev/scripts/target/rp2040.cfg -c "adapter speed 5000" -c "program build/esphub.elf verify reset exit"
+#$PICO_SDK_PATH/openocd/0.12.0+dev/openocd.exe -s /Users/destroyer/.pico-sdk/openocd/0.12.0+dev/scripts -f /home/destroyer/Projects/ESPhub/pico-sdk/openocd/0.12.0+dev/scripts/interface/cmsis-dap.cfg -f /home/destroyer/Projects/ESPhub/pico-sdk/openocd/0.12.0+dev/scripts/target/rp2040.cfg -c "adapter speed 5000" -c "program build/esphub.elf verify reset exit"
--- debug.sh
+++ debug.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 export PICO_SDK_PATH=/home/destroyer/Projects/ESPhub/pico-sdk
-export PATH_OPENOCD_SCRIPTS=$PICO_SDK_PATH/openocd/0.12.0+dev/scripts
-
-$PICO_SDK_PATH/openocd/0.12.0+dev/openocd.exe -s $PATH_OPENOCD_SCRIPTS -f $PATH_OPENOCD_SCRIPTS/interface/cmsis-dap.cfg -f $PATH_OPENOCD_SCRIPTS/target/rp2040.cfg -c "adapter speed 5000" -c "program build/esphub.elf verify reset exit"
+minicom -b 115200 -o -D /dev/ttyACM0
