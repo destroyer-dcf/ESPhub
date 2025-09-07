9458bcc46b1633fbc70e19bb403e390fb711fbcd
destroyer <destroyer-dcf@gmail.com>
2025-09-07 11:24:05 +0200

    add pico-sdk

--- firmware2pico.sh
+++ firmware2pico.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PICO_SDK_PATH=/home/destroyer/Projects/ESPhub/pico-sdk
-export PATH_OPENOCD_SCRIPTS=$PICO_SDK_PATH/openocd/0.12.0+dev/scripts
+export PICO_SDK_PATH=/home/destroyer/.pico-sdk/sdk/1.5.1
+export PATH_OPENOCD_SCRIPTS=/home/destroyer/Projects/ESPhub/pico-sdk/openocd/0.12.0+dev/scripts
 
-$PICO_SDK_PATH/openocd/0.12.0+dev/openocd.exe -s $PATH_OPENOCD_SCRIPTS -f $PATH_OPENOCD_SCRIPTS/interface/cmsis-dap.cfg -f $PATH_OPENOCD_SCRIPTS/target/rp2040.cfg -c "adapter speed 5000" -c "program build/esphub.elf verify reset exit"
+/home/destroyer/.pico-sdk/openocd/0.12.0+dev/openocd.exe -s $PATH_OPENOCD_SCRIPTS -f $PATH_OPENOCD_SCRIPTS/interface/cmsis-dap.cfg -f $PATH_OPENOCD_SCRIPTS/target/rp2040.cfg -c "adapter speed 5000" -c "program build/esphub.elf verify reset exit"
